3686842360e5e7ca7d4ceaeb66adf0a300d1e2c5
Octavian Mustafa <octaG-M@users.noreply.github.com>
2025-03-08 08:27:28 +0200

    Update compilarea_traducerii.ps1

--- scripturi/compilarea_traducerii.ps1
+++ scripturi/compilarea_traducerii.ps1
@@ -1,12 +1,15 @@
-$proiectul    = "C:\octavian\invatare_Python_2025\traducerea_documentatiei\proiectul_traducerii\Python-3.13.2\Doc"
-$doctree      = "$proiectul\build\doctrees"
-$sursa        = $proiectul
-$tinta        = "$proiectul\build"
-$executabilul = "C:\octavian\instalare_python\instalat\Scripts\sphinx-build.exe"
-$argumentele  = " -M html -d $doctree $sursa $tinta"
+$director_convenabil = "C:\octavian\invatare_Python_2025\traducerea_documentatiei\proiectul_traducerii"
+$proiectul           = "$director_convenabil\Python-3.13.2\Doc"
+$doctree             = "$proiectul\build\doctrees"
+$sursa               = $proiectul
+$tinta               = "$proiectul\build"
+$executabilul        = "C:\octavian\instalare_python\instalat\Scripts\sphinx-build.exe"
+$argumentele         = " -M html -d $doctree $sursa $tinta"
+$director_script     = Get-Location
 
-cd $sursa
-Start-Process -FilePath $executabilul -ArgumentList $argumentele -NoNewWindow
+Set-Location $sursa
+Start-Process -FilePath $executabilul -ArgumentList $argumentele -NoNewWindow -Wait
+Set-Location $director_script
 Set-ExecutionPolicy Restricted
 
               
